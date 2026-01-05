f948d22c3988b9c0a1b5cf6bf71c68075c5e876d
devqw010102 <qw010102@gmail.com>
2026-01-05 10:47:48 +0900

    learn 2 var, operator

---
+++ var/start.py
@@ -0,0 +1,21 @@
+# System.out.println => print
+print("Hello World1")
+print('Hello World2')
+
+# Define Variation : Not Keyword, Type
+name = '홍길동'
+print(name)
+
+# Operator : python add 2 more
+num1 = 2
+num2 = 3
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 % num2)
+
+print(num1 * num2)
+print(num1 ** num2) # pow(num1, num2) from java
+
+print(num1 / num2)  # Convert 2 float
+print(num1 // num2) # num1 / num 2 from Java
